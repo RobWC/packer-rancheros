1d3538c0e0be0d4e952b81f733e9c76534d02c46
RobWC <rwcameron@gmail.com>
2015-07-24 14:13:22 -0700

    update repo

--- packer/scripts/disk_install.sh
+++ packer/scripts/disk_install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo /usr/sbin/rancheros-install -f -d /dev/sda -v v0.2.1 -t generic -c /tmp/cloud_data.yml
+sudo /usr/sbin/rancheros-install -f -d /dev/sda -t generic -c /tmp/cloud_data.yml
